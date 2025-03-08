ba77533bbb2895b6bb41f1ff76efdd9c58635681
aidan073 <aidanbell073@gmail.com>
2025-03-08 00:08:46 -0500

    Input validation class added to Game.cs

--- GameManager/Game.cs
+++ GameManager/Game.cs
@@ -7,17 +7,34 @@ using models;
 
 namespace GameManager{
     class Prompts{
+        // For reading in prompts json file
         public string shipPlacement{get; set;}
     }
-    class Game{
 
+    class ValidateInputs{
+        // User console input validation
+        public static bool ValidateInput(int inputNum, string[] input){
+            switch(inputNum){
+                case 1:
+                    string[] coords = input[0].Split(',');
+                    if(input.Length < 2 || !int.TryParse(coords[0], out int x) || !int.TryParse(coords[1], out int y) || !Enum.TryParse(input[1], out Direction dir)){
+                        return false;
+                    }
+                    break;
+                case 2:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException($"Invalid inputNum: {inputNum}.");
+            }
+            return true;
+        }
+    }
+    
+    class Game{
         public static void Start(){
 
-            //add functionality for getting directions and bow location:
             string promptsPath = "prompts/UserPrompts.json";
             string rawPrompts;
-
-            
 
             if(File.Exists(promptsPath)){
                 rawPrompts = File.ReadAllText(promptsPath);
@@ -26,7 +43,6 @@ namespace GameManager{
                 throw new FileNotFoundException($"File {promptsPath} does not exist");
             }
             Prompts prompts = JsonSerializer.Deserialize<Prompts>(rawPrompts);
-
 
             //initialize ships
             var allShipTypes = Enum.GetValues(typeof(ShipType));
@@ -53,9 +69,16 @@ namespace GameManager{
                     default:
                         throw new ArgumentException($"Invalid ShipType {currShipType}.");
                 }
+                
+                // get user's ship placement
                 string[] prompt = {prompts.shipPlacement};
                 string[] userIn = UserInput.GetUserInput(prompt);
-                ships[idx] = new Ship(currShipType, length, userIn[1], userIn[0]);
+                while(!ValidateInputs.ValidateInput(1, userIn)){
+                    Console.WriteLine("Invalid input, please try again.");
+                    userIn = UserInput.GetUserInput(prompt);
+                }
+                string[] coords = userIn[0].Split(',');
+                ships[idx] = new Ship(currShipType, length, (Direction)Enum.Parse(typeof(Direction), userIn[1]), (int.Parse(coords[0]), int.Parse(coords[1])));
                 idx++;
             }
         }
